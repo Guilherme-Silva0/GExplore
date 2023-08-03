21f72ddd26b95d88b94095b9c3fa0fef16b97f89
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-03 19:39:26 -0300

    :ok_hand: style: Using color theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import TabNavigation from './src/navigations/TabNavigation'
+import { colors } from './src/theme/colors'
 
 export default function App() {
   return (
     <NavigationContainer>
       <TabNavigation />
-      <StatusBar style="dark" translucent backgroundColor="#fff" />
+      <StatusBar style="dark" translucent backgroundColor={colors.white} />
     </NavigationContainer>
   )
 }
