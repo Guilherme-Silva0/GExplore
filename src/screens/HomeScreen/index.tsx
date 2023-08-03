257dc1446d14ed32ec1d9b75b8b9d2e9fe967f62
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-03 19:41:42 -0300

    :sparkles: feat: Importing Home component

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,10 @@
-import { Text } from 'react-native'
-import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../../components/home/Header'
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Text>HomeScreen</Text>
+    <SafeAreaView>
+      <Header />
     </SafeAreaView>
   )
 }
