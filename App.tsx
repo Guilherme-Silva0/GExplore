71649a47e6047b8fb9a782b7039e155f7d77fb9e
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-04 19:34:02 -0300

    :sparkles: feat: Adding fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-import { NavigationContainer } from '@react-navigation/native'
+/* eslint-disable camelcase */
 import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
 import TabNavigation from './src/navigations/TabNavigation'
 import { colors } from './src/theme/colors'
+import { UserLocationProvider } from './src/context/UserLocationContext'
+import {
+  Outfit_400Regular,
+  Outfit_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/outfit'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Outfit_400Regular,
+    Outfit_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <NavigationContainer>
-      <TabNavigation />
-      <StatusBar style="dark" translucent backgroundColor={colors.white} />
+      <UserLocationProvider>
+        <TabNavigation />
+        <StatusBar style="dark" translucent backgroundColor={colors.white} />
+      </UserLocationProvider>
     </NavigationContainer>
   )
 }
