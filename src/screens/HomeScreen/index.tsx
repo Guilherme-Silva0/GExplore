da603e5bf23b3d9da3dd2270f588f5bffca0132f
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-04 19:34:46 -0300

    :sparkles: feat: Importing GoogleMapView component

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../../components/home/Header'
+import GoogleMapView from '../../components/home/GoogleMapView'
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Header />
+      <GoogleMapView />
     </SafeAreaView>
   )
 }
