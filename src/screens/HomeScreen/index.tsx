f6b908593fa318e5f5b9a432ca7a15eb2ad1c5dc
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-05 21:32:00 -0300

    :sparkles: feat: Importing CategoryList

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,12 +1,14 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../../components/home/Header'
 import GoogleMapView from '../../components/home/GoogleMapView'
+import CategoryList from '../../components/home/CategoryList'
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Header />
       <GoogleMapView />
+      <CategoryList />
     </SafeAreaView>
   )
 }
