1cac65d44a8263c8bb809d1750517fa8bfc84069
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-05 21:31:31 -0300

    :sparkles: feat: Creating CategoryItem constant

---
+++ src/constant/categoryList.ts
@@ -0,0 +1,20 @@
+export const categoryList = [
+  {
+    id: 1,
+    name: 'Posto de gasolina',
+    value: 'gas_station',
+    icon: require('../assets/gas.png'),
+  },
+  {
+    id: 2,
+    name: 'Restaurantes',
+    value: 'restaurant',
+    icon: require('../assets/food.png'),
+  },
+  {
+    id: 3,
+    name: 'Café',
+    value: 'cafe',
+    icon: require('../assets/cafe.png'),
+  },
+]
