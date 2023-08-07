4b9ac046fe455890abd7aa0c5eab0810307e7255
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-06 21:15:27 -0300

    :sparkles: feat: Taking a list of places through api

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,13 +2,40 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../../components/home/Header'
 import GoogleMapView from '../../components/home/GoogleMapView'
 import CategoryList from '../../components/home/CategoryList'
+import { mapsApi } from '../../services/api'
+import { useEffect, useState } from 'react'
+import PlaceList from '../../components/home/PlaceList'
+import { PlaceApiType } from '../../types/apiTypes'
+import { ScrollView } from 'react-native'
 
 export default function HomeScreen() {
+  const [placeList, setPlaceList] = useState<PlaceApiType[]>([])
+
+  useEffect(() => {
+    getNearbySearchPlaces()
+  }, [])
+  const getNearbySearchPlaces = () => {
+    mapsApi
+      .get('nearbysearch/json', {
+        params: {
+          location: '37.78825,-122.4324',
+          radius: '1500',
+          type: 'restaurant',
+        },
+      })
+      .then((res) => {
+        setPlaceList(res.data.results)
+        console.log('Req')
+      })
+  }
   return (
     <SafeAreaView>
-      <Header />
-      <GoogleMapView />
-      <CategoryList />
+      <ScrollView>
+        <Header />
+        <GoogleMapView />
+        <CategoryList />
+        {placeList && <PlaceList placeList={placeList} />}
+      </ScrollView>
     </SafeAreaView>
   )
 }
