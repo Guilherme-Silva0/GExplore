192f4f5169da73dbe3eefa359ef23dce18c8d5ca
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-06 21:11:10 -0300

    :sparkles: feat: Adding new category

--- src/constant/categoryList.ts
+++ src/constant/categoryList.ts
@@ -1,20 +1,50 @@
 export const categoryList = [
   {
     id: 1,
+    name: 'Restaurante',
+    value: 'restaurant',
+    icon: require('../assets/food.png'),
+  },
+  {
+    id: 2,
+    name: 'Café',
+    value: 'cafe',
+    icon: require('../assets/cafe.png'),
+  },
+  {
+    id: 3,
+    name: 'Academia',
+    value: 'gym',
+    icon: require('../assets/gym.png'),
+  },
+  {
+    id: 4,
+    name: 'Hospital',
+    value: 'hospital',
+    icon: require('../assets/hospital.png'),
+  },
+  {
+    id: 5,
     name: 'Posto de gasolina',
     value: 'gas_station',
     icon: require('../assets/gas.png'),
   },
   {
-    id: 2,
-    name: 'Restaurantes',
-    value: 'restaurant',
-    icon: require('../assets/food.png'),
+    id: 6,
+    name: 'Escola',
+    value: 'school',
+    icon: require('../assets/school.png'),
   },
   {
-    id: 3,
-    name: 'Café',
-    value: 'cafe',
-    icon: require('../assets/cafe.png'),
+    id: 7,
+    name: 'Banco',
+    value: 'bank',
+    icon: require('../assets/bank.png'),
+  },
+  {
+    id: 8,
+    name: 'Outros',
+    value: 'other',
+    icon: require('../assets/other.png'),
   },
 ]
