c9ea0691e2fca6aad341cc528d5a83e9c4180d35
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-07 21:28:01 -0300

    :sparkles: feat: Getting place by type

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -3,29 +3,43 @@ import Header from '../../components/home/Header'
 import GoogleMapView from '../../components/home/GoogleMapView'
 import CategoryList from '../../components/home/CategoryList'
 import { mapsApi } from '../../services/api'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import PlaceList from '../../components/home/PlaceList'
 import { PlaceApiType } from '../../types/apiTypes'
-import { ScrollView } from 'react-native'
+import { ActivityIndicator, ScrollView } from 'react-native'
+import { UserLocationContext } from '../../context/UserLocationContext'
+import { colors } from '../../theme/colors'
 
 export default function HomeScreen() {
   const [placeList, setPlaceList] = useState<PlaceApiType[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const { location } = useContext(UserLocationContext)
 
   useEffect(() => {
+    if (!location?.coords && placeList.length !== 0) return
     getNearbySearchPlaces()
-  }, [])
-  const getNearbySearchPlaces = () => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location])
+  const getNearbySearchPlaces = (selectedCategory = 'other') => {
+    setIsLoading(true)
     mapsApi
       .get('nearbysearch/json', {
         params: {
-          location: '37.78825,-122.4324',
+          location:
+            location?.coords.latitude + ',' + location?.coords.longitude,
           radius: '1500',
-          type: 'restaurant',
+          type: selectedCategory === 'other' ? null : selectedCategory,
         },
       })
       .then((res) => {
         setPlaceList(res.data.results)
         console.log('Req')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        setIsLoading(false)
       })
   }
   return (
@@ -33,8 +47,18 @@ export default function HomeScreen() {
       <ScrollView>
         <Header />
         <GoogleMapView />
-        <CategoryList />
-        {placeList && <PlaceList placeList={placeList} />}
+        <CategoryList
+          setSelectedCategory={(value) => getNearbySearchPlaces(value)}
+        />
+        {isLoading ? (
+          <ActivityIndicator
+            size={50}
+            color={colors.blue}
+            style={{ marginTop: 40 }}
+          />
+        ) : (
+          placeList && <PlaceList placeList={placeList} />
+        )}
       </ScrollView>
     </SafeAreaView>
   )
