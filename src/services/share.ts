4f016b4996ecef8a59e655a054212759578b4085
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-09 20:49:58 -0300

    :sparkles: feat: Creating share services

---
+++ src/services/share.ts
@@ -0,0 +1,9 @@
+import { Share } from 'react-native'
+import { PlaceApiType } from '../types/apiTypes'
+
+export function sharePlace(place: PlaceApiType) {
+  Share.share({
+    title: 'Compartilhe este lugar',
+    message: `Nome do lugar: ${place.name}\nEndereço: ${place.vicinity}`,
+  })
+}
