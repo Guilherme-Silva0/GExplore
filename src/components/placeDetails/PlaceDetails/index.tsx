0657bc3e5da56b68c62230315bdc536132fda289
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-08 21:22:41 -0300

    :sparkles: feat: Creating PlaceDetails component

---
+++ src/components/placeDetails/PlaceDetails/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { View, Text } from 'react-native'
+
+export default function PlaceDetails() {
+  return (
+    <View>
+      <Text>PlaceDetails</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
