10a0ec340d0402956baa41325d9b96315a148100
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-10 21:04:25 -0300

    :sparkles: feat: Creating GoogleMapViewFull component

---
+++ src/components/search/GoogleMapViewFull/index.tsx
@@ -0,0 +1,58 @@
+import { View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserLocationContext } from '../../../context/UserLocationContext'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import { styles } from './styles'
+import { PlaceApiType } from '../../../types/apiTypes'
+
+export default function GoogleMapViewFull({
+  placeList,
+}: {
+  placeList: PlaceApiType[]
+}) {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0552,
+    longitudeDelta: 0.0421,
+  })
+
+  const { location } = useContext(UserLocationContext)
+
+  useEffect(() => {
+    if (location) {
+      setMapRegion((oldMapRegion) => ({
+        ...oldMapRegion,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      }))
+    }
+  }, [location])
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        region={mapRegion}
+      >
+        <Marker title="Você" coordinate={mapRegion} />
+        {/* {placeList.map(
+            (place, index) =>
+              index <= 5 && (
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude: place.geometry.location.lat,
+                    longitude: place.geometry.location.lng,
+                  }}
+                  title={place.name}
+                  description={place.vicinity}
+                />
+              ),
+          )} */}
+      </MapView>
+    </View>
+  )
+}
---
+++ src/components/search/GoogleMapViewFull/styles.ts
@@ -0,0 +1,12 @@
+import { Dimensions, StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height * 0.85,
+  },
+})
