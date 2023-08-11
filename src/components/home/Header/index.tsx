a9e71f9a396f4eee050632a5ba6faff14dada123
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-08-10 21:03:45 -0300

    :sparkles: feat: Adding search functionality

--- src/components/home/Header/index.tsx
+++ src/components/home/Header/index.tsx
@@ -1,7 +1,11 @@
 import { View, Image, TextInput } from 'react-native'
 import { styles } from './style'
 
-export default function Header() {
+export default function Header({
+  handleSearch,
+}: {
+  handleSearch: (text: string) => void
+}) {
   return (
     <View style={styles.container}>
       <Image
@@ -9,7 +13,11 @@ export default function Header() {
         alt="logo image"
         style={styles.logo}
       />
-      <TextInput placeholder="Pesquisar" style={styles.searchBar} />
+      <TextInput
+        placeholder="Pesquise pelo nome do local"
+        style={styles.searchBar}
+        onChangeText={(text) => handleSearch(text)}
+      />
       <Image
         source={require('../../../assets/placeholder.jpg')}
         alt="user image"
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -3,7 +3,7 @@ import Header from '../../components/home/Header'
 import GoogleMapView from '../../components/home/GoogleMapView'
 import CategoryList from '../../components/home/CategoryList'
 import { mapsApi } from '../../services/api'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import PlaceList from '../../components/home/PlaceList'
 import { PlaceApiType } from '../../types/apiTypes'
 import { ActivityIndicator, ScrollView } from 'react-native'
@@ -12,6 +12,7 @@ import { colors } from '../../theme/colors'
 
 export default function HomeScreen() {
   const [placeList, setPlaceList] = useState<PlaceApiType[]>([])
+  const [searchTerm, setSearchTerm] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const { location } = useContext(UserLocationContext)
 
@@ -42,12 +43,23 @@ export default function HomeScreen() {
         setIsLoading(false)
       })
   }
+
+  const filteredPlaces = useMemo(() => {
+    if (searchTerm.trim().length > 2) {
+      return placeList.filter(
+        (place) =>
+          place?.name?.toLowerCase().includes(searchTerm.toLowerCase().trim()),
+      )
+    }
+    return placeList
+  }, [placeList, searchTerm])
+
   return (
     <SafeAreaView>
       <ScrollView>
-        <Header />
+        <Header handleSearch={setSearchTerm} />
         <GoogleMapView
-          placeList={placeList}
+          placeList={filteredPlaces}
           title="Principais locais proximos"
         />
         <CategoryList
@@ -60,7 +72,7 @@ export default function HomeScreen() {
             style={{ marginTop: 40 }}
           />
         ) : (
-          placeList && <PlaceList placeList={placeList} />
+          filteredPlaces && <PlaceList placeList={filteredPlaces} />
         )}
       </ScrollView>
     </SafeAreaView>
